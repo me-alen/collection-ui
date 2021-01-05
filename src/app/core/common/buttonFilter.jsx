2a4a36fee42f18faa0b7d2edcd2deb05464e83f6
Alen Francis <alenofficial10@gmail.com>
2021-01-05 21:30:20 +0530

    pagination WIP

--- src/app/core/common/buttonFilter.jsx
+++ src/app/core/common/buttonFilter.jsx
@@ -4,47 +4,56 @@ class ButtonFilter extends Component {
   state = {};
   render() {
     return (
-     <div className="filter-btn-section">
+      <div className="filter-btn-section">
         <div className="heading-wrapper d-flex align-items-center justify-content-between">
-            <h2 className="sub-heading">Collection Recommendations</h2>
-           <span class="icon-layout-switch cp"></span>
-          </div>
-          <div className="btn-container">
-          <ul className ="btn-list-wrapper">
+          <h2 className="sub-heading">Collection Recommendations</h2>
+          <span className="icon-layout-switch cp"></span>
+        </div>
+        <div className="btn-container">
+          <ul className="btn-list-wrapper">
             <li className="list-item">
               <span className="btn-text-wrapper">
-                <a className="list-link" href="">All</a>
-             <span className="badge">2769</span>
+                <a className="list-link" href="">
+                  All
+                </a>
+                <span className="badge">2769</span>
               </span>
-             </li>
-               <li className="list-item active">
+            </li>
+            <li className="list-item active">
               <span className="btn-text-wrapper">
-                <a className="list-link" href="">VLP_to_Pay</a>
-             <span className="badge">895</span>
+                <a className="list-link" href="">
+                  VLP_to_Pay
+                </a>
+                <span className="badge">895</span>
               </span>
-             </li>
-                <li className="list-item ">
+            </li>
+            <li className="list-item ">
               <span className="btn-text-wrapper">
-                <a className="list-link" href="">HP_to_Pay</a>
-             <span className="badge">746</span>
+                <a className="list-link" href="">
+                  HP_to_Pay
+                </a>
+                <span className="badge">746</span>
               </span>
-             </li>
-               <li className="list-item ">
+            </li>
+            <li className="list-item ">
               <span className="btn-text-wrapper">
-                <a className="list-link" href="">LP_to_Pay</a>
-             <span className="badge">590</span>
+                <a className="list-link" href="">
+                  LP_to_Pay
+                </a>
+                <span className="badge">590</span>
               </span>
-             </li>
-                <li className="list-item ">
+            </li>
+            <li className="list-item ">
               <span className="btn-text-wrapper">
-                <a className="list-link" href="">VHP_to_Pay</a>
-             <span className="badge">39</span>
+                <a className="list-link" href="">
+                  VHP_to_Pay
+                </a>
+                <span className="badge">39</span>
               </span>
-             </li>
-
+            </li>
           </ul>
-          </div>
-     </div>
+        </div>
+      </div>
     );
   }
 }
