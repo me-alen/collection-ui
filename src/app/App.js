ee4c5dff1ae0f6f476fada65fca0f4a01c876ac0
Alen Francis <alenofficial10@gmail.com>
2021-01-07 21:21:06 +0300

    utilising checkbox

--- src/app/App.js
+++ src/app/App.js
@@ -4,11 +4,13 @@ import LoginForm from "./Components/mainApp/loginForm/loginForm";
 import Home from "./Components/mainApp/homePortal";
 import "./App.scss";
 import DisplayChartsAndGraphs from "./Components/mainApp/Common/graphs/displayChartsAndGraph";
+import CheckboxContainer from './Components/mainApp/Common/checkboxContainer';
 
 function App() {
   return (
     <React.Fragment>
       <Switch>
+        <Route path="/checkbox" component={CheckboxContainer} />
         <Route path="/graphs" component={DisplayChartsAndGraphs}></Route>
         <Route path="/login" component={LoginForm} />
         <Route path="/home" component={Home} />
